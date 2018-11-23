845c82d9522050da8e84ec78cedf34e9b4bd5d35
风筝 <peijue.chen@aigens.cn>
2018-11-23 14:39:35 +0800

    Initial Commit

---
+++ internationalizationTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  internationalizationTest
+//
+//  Created by 陈培爵 on 2018/11/23.
+//  Copyright © 2018年 PeiJueChen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
